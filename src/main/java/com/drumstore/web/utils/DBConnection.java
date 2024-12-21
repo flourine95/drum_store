a7bcf1906179137bec072287c5138b96345008bd
Nguyen Phi Long <22130152@st.hcmuaf.edu.vn>
2024-12-21 16:50:06 +0700

    Refactor: Optimize database connection with HikariCP for connection pooling

--- src/main/java/com/drumstore/web/utils/DBConnection.java
+++ src/main/java/com/drumstore/web/utils/DBConnection.java
@@ -1,5 +1,7 @@
 package com.drumstore.web.utils;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.jdbi.v3.core.Jdbi;
 
 import java.io.IOException;
@@ -7,25 +9,55 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class DBConnection {
-    private static Jdbi jdbi;
+    private static volatile Jdbi jdbi;
+    private static HikariDataSource dataSource;
+
+    private DBConnection() {
+    }
 
     public static Jdbi getJdbi() {
         if (jdbi == null) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                Properties prop = new Properties();
-                InputStream inputStream = DBConnection.class.getClassLoader().getResourceAsStream("db.properties");
-                prop.load(inputStream);
+            synchronized (DBConnection.class) {
+                if (jdbi == null) {
+                    try {
+                        Properties prop = new Properties();
+                        InputStream inputStream = DBConnection.class.getClassLoader().getResourceAsStream("db.properties");
+                        prop.load(inputStream);
 
-                String url = prop.getProperty("db.url");
-                String user = prop.getProperty("db.username");
-                String password = prop.getProperty("db.password");
+                        String url = prop.getProperty("db.url");
+                        String user = prop.getProperty("db.username");
+                        String password = prop.getProperty("db.password");
 
-                jdbi = Jdbi.create(url, user, password);
-            } catch (IOException | ClassNotFoundException e) {
-                throw new RuntimeException("Could not load DB properties", e);
+                        HikariConfig config = getHikariConfig(url, user, password);
+
+                        dataSource = new HikariDataSource(config);
+
+                        jdbi = Jdbi.create(dataSource);
+                    } catch (IOException e) {
+                        throw new RuntimeException("Could not load DB properties", e);
+                    }
+                }
             }
         }
         return jdbi;
+    }
+
+    private static HikariConfig getHikariConfig(String url, String user, String password) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(user);
+        config.setPassword(password);
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setMaximumPoolSize(10); //(CPU cores * 2) + number of disks
+        config.setMinimumIdle(2);
+        config.setIdleTimeout(30000);
+        config.setMaxLifetime(1800000);
+        return config;
+    }
+
+    public static void closeDataSource() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
     }
 }
