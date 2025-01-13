e956ffb2be4af8d4b030b0e6ef6aceb04e57d809
Nguyen Phi Long <22130152@st.hcmuaf.edu.vn>
2025-01-13 16:31:30 +0700

    Refactor UserAddressService methods to return consistent types and remove redundant code

--- src/main/java/com/drumstore/web/services/UserAddressService.java
+++ src/main/java/com/drumstore/web/services/UserAddressService.java
@@ -8,29 +8,23 @@ import java.util.List;
 public class UserAddressService {
     private final UserAddressRepository userAddressRepository = new UserAddressRepository();
 
-    // Lấy danh sách tất cả địa chỉ
     public List<UserAddress> all() {
         return userAddressRepository.all();
-    }
-
-    // Tìm địa chỉ theo ID
-    public UserAddress getUserAddressById(int id) {
-        return userAddressRepository.find(id);
-    }
-
-    public UserAddress find(int id) {
-        return userAddressRepository.find(id);
     }
 
     public boolean create(UserAddress address) {
         return userAddressRepository.create(address);
     }
 
-    public boolean update(int id, UserAddress address) {
-        return userAddressRepository.update(id, address);
+    public UserAddress find(int id) {
+        return userAddressRepository.find(id);
     }
 
-    public boolean deleteAddress(int id) {
+    public int update(UserAddress address) {
+        return userAddressRepository.update(address);
+    }
+
+    public int delete(int id) {
         return userAddressRepository.delete(id);
     }
 
