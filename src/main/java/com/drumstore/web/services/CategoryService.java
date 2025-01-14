32ed93a9b7a4f7d7f507a06b28487f5b5cf324c7
Nguyen Phi Long <22130152@st.hcmuaf.edu.vn>
2025-01-14 16:47:45 +0700

    Remove register css

--- src/main/java/com/drumstore/web/services/CategoryService.java
+++ src/main/java/com/drumstore/web/services/CategoryService.java
@@ -1,4 +1,17 @@
 package com.drumstore.web.services;
 
+import com.drumstore.web.models.Category;
+import com.drumstore.web.repositories.CategoryRepository;
+
+import java.util.List;
+
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService() {
+        this.categoryRepository = new CategoryRepository();
+    }
+    public List<Category> getCategories(int limit) {
+        return categoryRepository.getCategories(limit);
+    }
 }
