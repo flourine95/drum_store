d9061592a97d73cad0af6923481b5be9c9e8aedd
XuanDong <22130043@st.hcmuaf.edu.vn>
2025-04-15 21:09:59 +0700

    Feature (manager Voucher): new voucher, delete voucher, update voucher, view voucher

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,7 @@
 import com.drumstore.web.dto.RoleDTO;
 import com.drumstore.web.repositories.PermissionRepository;
 import com.drumstore.web.repositories.RoleRepository;
+import com.drumstore.web.repositories.UserRepository;
 
 public class Test {
     public static void main(String[] args) {
@@ -8,7 +9,7 @@ public class Test {
 //        System.out.println(new RoleRepository().createRole(RoleDTO.builder().name("ADMIN1").description("asd").build()));
         new RoleRepository().getAllRoles().forEach(System.out::println);
         RoleDTO roleRequest = RoleDTO.builder().id(null).name("ADMIN").description("description").build();
-        System.out.println( new RoleRepository().updateRole(roleRequest));
+        System.out.println( new UserRepository().login("voxuandong14052004@gmail.com", "Dong@14052004"));
     }
 
 }
