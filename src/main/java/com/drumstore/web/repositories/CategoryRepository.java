8de71080b67f7f3a72be34baae452052f51a982c
Nguyen Phi Long <22130152@st.hcmuaf.edu.vn>
2025-05-03 13:06:53 +0700

    Implement Category Management Features
    - Introduced `CategoryManagerController` for handling category-related actions including create, edit, update, and delete.
    - Enhanced `CategoryDTO` to include a `createdAt` timestamp for better tracking.
    - Updated `CategoryRepository` with methods for creating, updating, finding, and deleting categories.
    - Improved `CategoryService` to manage category operations and interactions with the repository.
    - Revamped JSP pages for category creation and editing, incorporating a modern UI with image upload functionality and user-friendly forms.
    - Enhanced the categories index page with a responsive layout, action buttons, and delete confirmation modal for better user experience.

--- src/main/java/com/drumstore/web/repositories/CategoryRepository.java
+++ src/main/java/com/drumstore/web/repositories/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.drumstore.web.repositories;
 
+import com.drumstore.web.dto.CategoryDTO;
 import com.drumstore.web.models.Category;
 import com.drumstore.web.models.Product;
 import com.drumstore.web.utils.DBConnection;
@@ -16,5 +17,48 @@ public class CategoryRepository {
                         .mapToBean(Category.class)
                         .list()
         );
+    }
+
+    public void create(CategoryDTO category) {
+        jdbi.useHandle(handle -> {
+            handle.createUpdate("INSERT INTO categories (name, description, image) VALUES (:name, :description, :image)")
+                    .bindBean(category)
+                    .execute();
+        });
+    }
+
+    public CategoryDTO findById(Integer id) {
+        return jdbi.withHandle(handle ->
+                handle.createQuery("SELECT * FROM categories WHERE id = :id")
+                        .bind("id", id)
+                        .mapToBean(CategoryDTO.class)
+                        .findFirst()
+                        .orElse(null)
+        );
+    }
+
+    public void update(CategoryDTO category) {
+        jdbi.useHandle(handle -> {
+            handle.createUpdate("UPDATE categories SET name = :name, description = :description, image = :image WHERE id = :id")
+                    .bindBean(category)
+                    .execute();
+        });
+    }
+
+    public boolean hasAssociatedProducts(Integer id) {
+        return jdbi.withHandle(handle ->
+                handle.createQuery("SELECT COUNT(*) FROM products WHERE categoryId = :id")
+                        .bind("id", id)
+                        .mapTo(Integer.class)
+                        .one() > 0
+        );
+    }
+
+    public void delete(Integer id) {
+        jdbi.useHandle(handle -> {
+            handle.createUpdate("DELETE FROM categories WHERE id = :id")
+                    .bind("id", id)
+                    .execute();
+        });
     }
 }
